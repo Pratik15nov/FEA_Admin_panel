c41d131f873fc8267368049add1a4c67230acc0d
FEAvansh <vansh.frontendarmy@gmail.com>
2022-08-16 10:45:02 +0530

    "First commit"

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
