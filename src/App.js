28257529eef3fb5a6ed2037efc02a4207c6f5763
FEAprince <104749623+FEAprince@users.noreply.github.com>
2022-08-16 15:44:53 +0530

    [SIDEBAR]

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Login />} />
+        <Route exact path="/Navbar" element={<Navbar />} />
       </Routes>
     </BrowserRouter>
   );
