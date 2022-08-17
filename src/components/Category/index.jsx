b898aead3172a062d449de059a299adc3337e235
FEAvansh <vansh.frontendarmy@gmail.com>
2022-08-17 16:00:28 +0530

    "issues "

--- src/components/Category/index.jsx
+++ src/components/Category/index.jsx
@@ -27,32 +27,6 @@ import AppRegistrationTwoToneIcon from "@mui/icons-material/AppRegistrationTwoTo
 import { categoryHandlerData } from "../../service/Auth.Service";
 import { listBody } from "../../utils/Helper";
 
-function createData(name, calories, fat, carbs, protein) {
-  return {
-    name,
-    calories,
-    fat,
-    carbs,
-    protein,
-  };
-}
-
-const rows = [
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Donut", 452, 25.0, 51, 4.9),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-  createData("Honeycomb", 408, 3.2, 87, 6.5),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Jelly Bean", 375, 0.0, 94, 0.0),
-  createData("KitKat", 518, 26.0, 65, 7.0),
-  createData("Lollipop", 392, 0.2, 98, 0.0),
-  createData("Marshmallow", 318, 0, 81, 2.0),
-  createData("Nougat", 360, 19.0, 9, 37.0),
-  createData("Oreo", 437, 18.0, 63, 4.0),
-];
-
 function descendingComparator(a, b, orderBy) {
   if (b[orderBy] < a[orderBy]) {
     return -1;
@@ -85,37 +59,31 @@ function stableSort(array, comparator) {
 
 const headCells = [
   {
-    // id: "name",
     id: "categories",
     numeric: false,
     disablePadding: true,
     label: "Categories",
   },
   {
-    // id: "calories",
     id: "createdAt",
     numeric: true,
     disablePadding: false,
     label: "Created At",
   },
   {
-    // id: "fat",
     id: "status",
     numeric: false,
     disablePadding: false,
     label: "Status",
   },
   {
-    // id: "carbs",
     id: "updatedProduct",
     numeric: true,
     disablePadding: false,
     label: "Update product",
   },
   {
-    // id: "protein",
     id: "deleteProduct",
-
     numeric: true,
     disablePadding: false,
     label: "Delete product",
@@ -262,10 +230,7 @@ export default function Category() {
     setCategoryData(response);
   };
 
-  console.log(
-    "DATA",
-    categorydata
-  );
+  console.log("DATA", categorydata);
 
   const handleRequestSort = (event, property) => {
     const isAsc = orderBy === property && order === "asc";
@@ -275,7 +240,7 @@ export default function Category() {
 
   const handleSelectAllClick = (event) => {
     if (event.target.checked) {
-      const newSelected = rows.map((n) => n.name);
+      const newSelected = categorydata.map((n) => n.categoryName);
       setSelected(newSelected);
       return;
     }
@@ -384,7 +349,9 @@ export default function Category() {
                         </Box>
                       </TableCell>
                       <TableCell align="right">{row.createdAt}</TableCell>
-                      <TableCell align="right">{row.isActive ? 'True' : 'False'}</TableCell>
+                      <TableCell align="right">
+                        {row.isActive ? "True" : "False"}
+                      </TableCell>
                       <TableCell align="right">
                         {<AppRegistrationTwoToneIcon sx={{ color: "green" }} />}
                       </TableCell>
