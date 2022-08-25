1215848d125094990749ddffb1982453ed5c8e19
FEAvansh <vansh.frontendarmy@gmail.com>
2022-08-25 12:43:51 +0530

    "updated chnnges"

--- src/js/actions/index.jsx
+++ src/js/actions/index.jsx
@@ -1,9 +1,16 @@
 export function fetchCategoryList(payload) {
-  return { type: 'FETCH_CATEGORY', payload};
+  return { type: "FETCH_CATEGORY", payload };
 }
 
 export function categoryStatusChange(payload) {
-  return { type: 'CHANGE_CATEGORY_STATUS', payload};
+  return { type: "CHANGE_CATEGORY_STATUS", payload };
+}
+
+export function onDeletion(payload) {
+  return { type: "ON_DELETION", payload };
+}
+export function onSearch(payload) {
+  return { type: "ON_SEARCH", payload };
 }
 
 export function fetchCategoryListFailure() {
--- src/js/middleware/index.jsx
+++ src/js/middleware/index.jsx
@@ -1,56 +1,66 @@
-import { categoryHandlerData, categoryStatus } from '../../service/Auth.Service';
-import { 
-    fetchCategoryList,
-    fetchCategoryListFailure,
-    fetchCategoryListSuccess,
-} from '../actions'
+import {
+  categoryHandlerData,
+  categoryStatus,
+  categoryDelete,
+} from "../../service/Auth.Service";
+import {
+  fetchCategoryList,
+  fetchCategoryListFailure,
+  fetchCategoryListSuccess,
+} from "../actions";
 
 export const loggerMiddleware = (store) => (next) => (action) => {
   try {
     switch (action.type) {
-        case 'FETCH_CATEGORY':
-            categoryHandlerData(action.payload).then(res => {
-                if(res.success) {
-                    store.dispatch(fetchCategoryListSuccess(res));
-                } else {
-                    store.dispatch(fetchCategoryListFailure());    
-                }
-            }).catch(err => {
-                store.dispatch(fetchCategoryListFailure());
-            })
+      case "FETCH_CATEGORY":
+        categoryHandlerData(action.payload)
+          .then((res) => {
+            if (res.success) {
+              store.dispatch(fetchCategoryListSuccess(res));
+            } else {
+              store.dispatch(fetchCategoryListFailure());
+            }
+          })
+          .catch((err) => {
+            alert("ERROR OCCURED");
+            store.dispatch(fetchCategoryListFailure());
+          });
         break;
-        case 'CHANGE_CATEGORY_STATUS':
-            categoryStatus(action.payload.id, action.payload.body).then(res => {
-                console.log(res);
-                if(res.success) {
-                    store.dispatch(fetchCategoryList(action.payload.defaultPayload));
-                } else {
-                    store.dispatch(fetchCategoryList(action.payload.defaultPayload));
-                }
-            }).catch(err => {
-                store.dispatch(fetchCategoryListFailure());
-            })
-            break;
+      case "CHANGE_CATEGORY_STATUS":
+        categoryStatus(action.payload.id, action.payload.body)
+          .then((res) => {
+            console.log(res);
+            if (res.success) {
+              store.dispatch(fetchCategoryList(action.payload.defaultPayload));
+            } else {
+              alert("RESPONSE:FALSE => STATUS NOT CHANGED");
+              store.dispatch(fetchCategoryList(action.payload.defaultPayload));
+            }
+          })
+          .catch((err) => {
+            alert("ERROR OCCURED");
+            store.dispatch(fetchCategoryListFailure());
+          });
+        break;
+      case "ON_DELETION":
+        categoryDelete(action.payload.id)
+          .then((res) => {
+            if (res.success) {
+              store.dispatch(fetchCategoryList(action.payload.defaultPayload));
+            } else {
+              alert("RESPONSE:FALSE => DELETION NOT CHANGED");
+              store.dispatch(fetchCategoryList(action.payload.defaultPayload));
+            }
+          })
+          .catch((error) => {
+            alert("ERROR OCCURED");
+            store.dispatch(fetchCategoryListFailure());
+          });
+        break;
       default:
         return next(action);
     }
   } catch (error) {
     alert(error);
   }
-
-  //   try {
-  //     if (action.type === "FETCH_CATEGORY_SUCCESS") {
-  //       console.log("action.type: ", action.type);
-
-  //       next(action);
-  //     } else if (action.type === "FETCH_PRODUCT_SUCCESS") {
-  //       console.log("action.type: ", action.type);
-  //       next(action);
-  //     } else {
-  //       console.log("NOT_SUCCESS");
-  //       next(action);
-  //     }
-  //   } catch (error) {
-  //     alert(error);
-  //   }
 };
