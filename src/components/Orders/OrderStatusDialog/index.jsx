44b680df646ccd04a288c742c97f3ba859314e16
FEAvansh <vansh.frontendarmy@gmail.com>
2022-08-30 11:26:01 +0530

    "chnegs"

--- src/components/Orders/OrderStatusDialog/index.jsx
+++ src/components/Orders/OrderStatusDialog/index.jsx
@@ -5,14 +5,20 @@ import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 
 import { useForm, Controller } from "react-hook-form";
+import { useEffect } from "react";
 
 export function OrderStatusDialog(props) {
-  const { handleSubmit, control } = useForm({
+  const { handleSubmit, control, reset } = useForm({
     defaultValues: {
       RadioGroup: "",
     },
   });
 
+  useEffect(() => {
+    reset({ RadioGroup: props.dialogData.orderStatus });
+  }, [props.dialogData.orderStatus]);
+
+  console.log("PROPS", props.dialogData.orderStatus);
   const handleClose = (data) => {
     console.log("data: ", data);
     props.onClose(props.selectedValue);
@@ -21,16 +27,25 @@ export function OrderStatusDialog(props) {
     <Dialog onClose={handleClose} open={props.open}>
       <DialogTitle>OrderStatus</DialogTitle>
 
-      <form onSubmit={handleSubmit((data) => console.log("DATA", data))}>
+      <form onSubmit={handleSubmit(handleClose)}>
         <Controller
           render={({ field }) => (
-            <RadioGroup aria-label="gender" {...field}>
+            <RadioGroup aria-label="orderStatus" {...field}>
               <FormControlLabel
-                value="female"
+                value="PLACED"
                 control={<Radio />}
-                label="Female"
+                label="PLACED"
               />
-              <FormControlLabel value="male" control={<Radio />} label="Male" />
+              <FormControlLabel
+                value="DISPATCHED"
+                control={<Radio />}
+                label="DISPATCHED"
+              />
+              <FormControlLabel
+                value="RECEIVED"
+                control={<Radio />}
+                label="RECEIVED"
+              />
             </RadioGroup>
           )}
           name="RadioGroup"
