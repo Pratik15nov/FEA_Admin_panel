423ef0b820450ba0af040fcb46583aaf3e70e7fd
Kedar <hardik.frontendarmy@gmail.com>
2022-08-31 19:03:00 +0530

    []

---
+++ src/components/Dashboard/Dashboard.style.jsx
@@ -0,0 +1,8 @@
+import Card from '@mui/material/Card';
+import {
+    styled,
+} from "@mui/material";
+export const CardFrist = styled(Card)(({ theme }) => ({
+    backgroundColor: "#FA58B6",
+    padding:20
+}));
