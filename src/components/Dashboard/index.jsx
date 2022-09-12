9e364a5892f7e907a44b5d3c6379955c035b7670
Kedar <hardik.frontendarmy@gmail.com>
2022-09-12 14:32:02 +0530

    [skeleton added]

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -1,114 +1,55 @@
-import * as React from 'react';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
-import {
-  CardFrist,
-  MainBody
-} from "./Dashboard.style";
+import * as React from "react";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import { CardFrist, MainBody } from "./Dashboard.style";
 export default function Dashboard() {
-  return <MainBody> <Grid container spacing={2}>
-    <Grid xs={8}>
-      <CardFrist>
-        <Grid container>
-          <Grid container xs={8}>
-            <CardContent>
-              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                Word of the Day
-              </Typography>
-              <Typography variant="h5" component="div">
-                Welcome back!
-              </Typography>
-              <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                Best seller of the month You have done 57.6% more sales today.
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small" variant="contained">Go now</Button>
-            </CardActions>
-          </Grid>
-          <Grid container xs={4}><img
-            src="/images/banner1.png"
-            alt="baneer"
-            loading="lazy"
-            width="200px"
-            height="200px"
-          /></Grid>
+  return (
+    <MainBody>
+      {" "}
+      <Grid container spacing={2}>
+        <Grid xs={8}>
+          <CardFrist>
+            <Grid container>
+              <Grid container xs={8}>
+                <CardContent>
+                  <Typography
+                    sx={{ fontSize: 14 }}
+                    color="text.secondary"
+                    gutterBottom
+                  >
+                    Word of the Day
+                  </Typography>
+                  <Typography variant="h5" component="div">
+                    Welcome back!
+                  </Typography>
+                  <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                    Best seller of the month You have done 57.6% more sales
+                    today.
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" variant="contained">
+                    Go now
+                  </Button>
+                </CardActions>
+              </Grid>
+              <Grid container xs={4}>
+                <img
+                  src="/images/banner1.png"
+                  alt="baneer"
+                  loading="lazy"
+                  width="200px"
+                  height="200px"
+                />
+              </Grid>
+            </Grid>
+          </CardFrist>
         </Grid>
-      </CardFrist>
-    </Grid>
-    <Grid xs={4}>
-      <CardFrist sx={{ minWidth: 275 }}>
-        <CardContent>
-
-        </CardContent>
-        <CardActions>
-          <Button size="small">Learn More</Button>
-        </CardActions>
-      </CardFrist>
-    </Grid>
-
-  </Grid >
-    <Grid container >
-      <Grid xs={4}>
-        <Grid container>
-          <Grid container xs={8}>
-            <CardContent>
-              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-              Product Sold
-              </Typography>
-              <Typography variant="h5" component="div">
-                Welcome back!
-              </Typography>
-              <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                Best seller of the month You have done 57.6% more sales today.
-              </Typography>
-            </CardContent>
-
-          </Grid>
-          <Grid container xs={4}></Grid>
-        </Grid>
+        <Grid xs={4}></Grid>
       </Grid>
-      <Grid xs={4}>
-        <Grid container>
-          <Grid container xs={8}>
-            <CardContent>
-              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-              Total Balance
-              </Typography>
-              <Typography variant="h5" component="div">
-                Welcome back!
-              </Typography>
-              <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                Best seller of the month You have done 57.6% more sales today.
-              </Typography>
-            </CardContent>
-
-          </Grid>
-          <Grid container xs={4}></Grid>
-        </Grid>
-      </Grid>
-      <Grid xs={4}>
-        <Grid container>
-          <Grid container xs={8}>
-            <CardContent>
-              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-              Sales Profit
-              </Typography>
-              <Typography variant="h5" component="div">
-                Welcome back!
-              </Typography>
-              <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                Best seller of the month You have done 57.6% more sales today.
-              </Typography>
-            </CardContent>
-
-          </Grid>
-          <Grid container xs={4}></Grid>
-        </Grid>
-      </Grid>
-
-    </Grid></MainBody>
+    </MainBody>
+  );
 }
