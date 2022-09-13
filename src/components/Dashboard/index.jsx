667c694e39a0f77e6facdeee2a5b92f8c7da4c95
Kedar <hardik.frontendarmy@gmail.com>
2022-09-13 16:05:39 +0530

    []

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -1,57 +1,250 @@
-import * as React from "react";
-import CardActions from "@mui/material/CardActions";
+import { React, useEffect } from "react";
 import CardContent from "@mui/material/CardContent";
-import Button from "@mui/material/Button";
+// import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
-import { CardFrist, MainBody } from "./Dashboard.style";
-import { Alert } from "@mui/material";
+import {
+  CardFrist,
+  CardOne,
+  CardTwo,
+  ContainerTwo,
+  MainBody,
+} from "./Dashboard.style";
+// import { Alert } from "@mui/material";
+import {
+  fetchCategoryList,
+  fetchCustomersList,
+  fetchOrderList,
+  fetchProductList,
+} from "../../js/actions";
+import { useDispatch, useSelector } from "react-redux";
+import { listBody } from "../../utils/Helper";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend,
+  ArcElement,
+} from "chart.js";
+import { Doughnut, Line } from "react-chartjs-2";
+import faker from "faker";
+ChartJS.register(
+  ArcElement,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend
+);
+
 export default function Dashboard() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    getDashboardData(); // eslint-disable-next-line
+  }, []);
+  // const state = useSelector((state) => state);
+  const orderCount = useSelector((state) => state.order.totalCount);
+  const productCount = useSelector((state) => state.product.totalCount);
+  const customerCount = useSelector((state) => state.customers.totalCount);
+  const categoryCount = useSelector((state) => state.category.totalCount);
+  // console.log("STATE", state);
+  const data = {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3, 6, 5],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.7)",
+          "rgba(54, 162, 235, 0.7)",
+          "rgba(255, 206, 86, 0.7)",
+          "rgba(75, 192, 192, 0.7)",
+          "rgba(153, 102, 255, 0.7)",
+          "rgba(255, 159, 64, 0.7)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+  const labels = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+  ];
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+    },
+    scales: {
+      x: {
+        grid: {
+          display: false,
+        },
+      },
+      y: {
+        grid: {
+          display: false,
+        },
+      },
+    },
+  };
+  const datas = {
+    labels,
+    datasets: [
+      {
+        fill: true,
+        label: "Sales",
+        data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+        borderColor: "rgb(26, 26, 64)",
+        backgroundColor: "rgba(26, 26, 64,0.2)",
+      },
+    ],
+  };
+  const getDashboardData = () => {
+    try {
+      dispatch(fetchOrderList(listBody({ where: null, perPage: 10, page: 1 })));
+      dispatch(
+        fetchProductList(listBody({ where: null, perPage: 10, page: 1 }))
+      );
+      dispatch(
+        fetchCustomersList(listBody({ where: null, perPage: 10, page: 1 }))
+      );
+      dispatch(
+        fetchCategoryList(listBody({ where: null, perPage: 10, page: 1 }))
+      );
+    } catch (err) {
+      alert(err);
+    }
+  };
+
   return (
     <>
       {/* <Alert severity="error">Session token expired!</Alert> */}
       <MainBody>
         <Grid container spacing={2}>
-          <Grid xs={8}>
+          <Grid xs={3}>
             <CardFrist>
               <Grid container>
-                <Grid container xs={8}>
+                <Grid container xs={10}>
                   <CardContent>
-                    <Typography
-                      sx={{ fontSize: 14 }}
-                      color="text.secondary"
-                      gutterBottom
-                    >
-                      Word of the Day
-                    </Typography>
-                    <Typography variant="h5" component="div">
-                      Welcome back!
-                    </Typography>
-                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                      Best seller of the month You have done 57.6% more sales
-                      today.
+                    <CardTwo variant="h6" component="div">
+                      Total Orders
+                    </CardTwo>
+                    <CardOne variant="h5" component="div">
+                      {orderCount}
+                    </CardOne>
+                    <Typography color="text.secondary">
+                      +2.6% than last week
                     </Typography>
                   </CardContent>
-                  <CardActions>
-                    <Button size="small" variant="contained">
-                      Go now
-                    </Button>
-                  </CardActions>
-                </Grid>
-                <Grid container xs={4}>
-                  <img
-                    src="/images/banner1.png"
-                    alt="baneer"
-                    loading="lazy"
-                    width="200px"
-                    height="200px"
-                  />
                 </Grid>
               </Grid>
             </CardFrist>
           </Grid>
-          <Grid xs={4}></Grid>
+          <Grid xs={3}>
+            <CardFrist>
+              <Grid container>
+                <Grid container xs={10}>
+                  <CardContent>
+                    <CardTwo variant="h6" component="div">
+                      Total Products
+                    </CardTwo>
+                    <CardOne variant="h5" component="div">
+                      {productCount}
+                    </CardOne>
+                    <Typography color="text.secondary">
+                      +2.6% than last week
+                    </Typography>
+                  </CardContent>
+                </Grid>
+              </Grid>
+            </CardFrist>
+          </Grid>
+          <Grid xs={3}>
+            <CardFrist>
+              <Grid container>
+                <Grid container xs={10}>
+                  <CardContent>
+                    <CardTwo variant="h6" component="div">
+                      Total Customers
+                    </CardTwo>
+                    <CardOne variant="h5" component="div">
+                      {customerCount}
+                    </CardOne>
+                    <Typography color="text.secondary">
+                      +2.6% than last week
+                    </Typography>
+                  </CardContent>
+                </Grid>
+              </Grid>
+            </CardFrist>
+          </Grid>
+          <Grid xs={3}>
+            <CardFrist>
+              <Grid container>
+                <Grid container xs={10}>
+                  <CardContent>
+                    <CardTwo variant="h6" component="div">
+                      Total Category
+                    </CardTwo>
+                    <CardOne variant="h5" component="div">
+                      {categoryCount}
+                    </CardOne>
+                    <Typography color="text.secondary">
+                      +2.6% than last week
+                    </Typography>
+                  </CardContent>
+                </Grid>
+              </Grid>
+            </CardFrist>
+          </Grid>
         </Grid>
+        <ContainerTwo container spacing={2}>
+          <Grid xs={4}>
+            <CardFrist>
+              <CardContent>
+                <CardTwo variant="h6" component="div">
+                  Orders by Products
+                </CardTwo>
+                <Doughnut data={data} />
+              </CardContent>
+            </CardFrist>
+          </Grid>
+          <Grid xs={8}>
+            <CardFrist>
+              <CardContent>
+                <CardTwo variant="h6" component="div">
+                  Total Sales
+                </CardTwo>
+                <Line options={options} data={datas} />
+              </CardContent>
+            </CardFrist>
+          </Grid>
+        </ContainerTwo>
       </MainBody>
     </>
   );
